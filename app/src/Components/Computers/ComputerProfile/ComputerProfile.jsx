62d82e340f4748151f63de1129c3bb51a919efe5
MaksimWeb <sayamonkoh@gmail.com>
2021-03-08 17:34:37 +1000

    checking list

--- app/src/Components/Computers/ComputerProfile/ComputerProfile.jsx
+++ app/src/Components/Computers/ComputerProfile/ComputerProfile.jsx
@@ -8,19 +8,24 @@ import style from './ComputerProfile.module.css'
 export const ComputerProfile = () => {
 
     const [computerApps, setComputerApps] = useState([])
+    const [applications, setApplications] = useState([])
 
     let makeFile = axios.get('http://127.0.0.1:8000/computers/script/')
     let getComputerInfo = axios.get('http://127.0.0.1:8000/computers/')
+    let getApplicationsInfo = axios.get('http://127.0.0.1:8000/computers/applications')
 
 
     let getInfo = () => axios
-        .all([makeFile, getComputerInfo])
-        .then(axios.spread((...responses) => setComputerApps(responses[1].data)))
+        .all([makeFile, getComputerInfo, getApplicationsInfo])
+        .then(axios.spread((...responses) => {
+            setComputerApps(responses[1].data)
+            setApplications(responses[2].data)
+        }))
 
     return (
         <div className={style.listBlock}>
-            <TableContainer component={Paper}>
-                <Table size="small" aria-label="a dense table">
+            <TableContainer className={style.tableContainer} component={Paper}>
+                <Table className={style.computerTable} size="small" aria-label="a dense table">
                     <TableHead>
                         <TableRow>
                             <TableCell align="left">Название ПК</TableCell>
@@ -45,6 +50,22 @@ export const ComputerProfile = () => {
                                 <TableCell align="left">{row.physical_memory}</TableCell>
                                 <TableCell
                                     align="left">{row.video_driver}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+                <Table size="small" aria-label="a dense table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell align="left">Название программы</TableCell>
+                            <TableCell align="left">Версия программы</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {applications.map((row) => (
+                            <TableRow key={row.application_name}>
+                                <TableCell align="left">{row.application_name}</TableCell>
+                                <TableCell align="left">{row.application_version}</TableCell>
                             </TableRow>
                         ))}
                     </TableBody>
--- app/src/Components/Redux/computers-reducer.js
+++ app/src/Components/Redux/computers-reducer.js
@@ -1,5 +1,6 @@
 
 let initialState = {
+    computers: [],
 
 }
 
